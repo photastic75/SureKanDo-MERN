9a27e0e658424ee3c5a57508bfcbb23f78e0ae44
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-11-07 20:14:00 +0000

    first commit

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+ } from 'reactstrap';
+
+ class AppNavBar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOepn
+        });
+    }
+
+    render() {
+        return (
+            <div>
+              <Navbar color="dark" dark expand="sm" className="mb-5">
+                <NavbarBrand href="/">Shopping List</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                  <Nav className="ml-auto" navbar>
+                    <NavItem>
+                      <NavLink href="https://github.com/photastic75">GitHub</NavLink>
+                    </NavItem>
+                  </Nav>
+                </Collapse>
+              </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNavBar;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://mern-charlie-fu:Job315tc@mern-shopping-izkur.mongodb.net/test?retryWrites=true&w=majority'
+}
